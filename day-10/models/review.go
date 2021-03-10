1ed90aaf843476e4f2bbd10fa8e5bd9fc176ecf2
Sahlan Nasution <sahlan.nasution07@gmail.com>
2021-03-10 10:40:27 +0700

    fix models review

--- day-10/models/review.go
+++ day-10/models/review.go
@@ -4,12 +4,12 @@ import "gorm.io/gorm"
 
 // Reviews struct
 type Reviews struct {
-	ID      uint   `gorm:"primarykey, autoIncrement" json:"id"`
-	Review  string `json:"review"`
-	Rate    int    `json:"rate"`
-	UserID  int    `json:"user_id"`
-	Users   Users  `gorm:"foreignKey:ID" json:"user"`
-	MovieID int    `json:"movie_id"`
-	Movies  Movies `gorm:"foreignKey:ID" json:"movie"`
+	ID       uint   `gorm:"primarykey, autoIncrement" json:"id"`
+	Review   string `json:"review"`
+	Rate     int    `json:"rate"`
+	UsersID  uint   `json:"users_id"`
+	Users    Users  `gorm:"foreignKey:UsersID" json:"users"`
+	MoviesID uint   `json:"movies_id"`
+	Movies   Movies `gorm:"foreignKey:MoviesID" json:"movies"`
 	gorm.Model
 }
